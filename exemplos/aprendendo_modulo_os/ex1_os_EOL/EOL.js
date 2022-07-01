becfd6748190ab92f48bf2d1cc14896d7a4fcfb7
Thiago de Almeida Pereira <thiago_ap_@hotmail.com>
2022-07-01 02:29:57 -0300

    Treinando o modulo os, foi treinado os.EOL e os.arch()

---
+++ exemplos/aprendendo_modulo_os/ex1_os_EOL/EOL.js
@@ -0,0 +1,3 @@
+const os = require('os');
+
+console.log(`O atributo EOL é uma quebra de linha.${os.EOL}A quebra de linha funcionou`);
---
+++ exemplos/aprendendo_modulo_os/ex2_os_arch/arch.js
@@ -0,0 +1,3 @@
+const os = require('os');
+
+console.log(`Esse linux é ${os.arch()}`);
